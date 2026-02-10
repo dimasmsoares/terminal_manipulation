a5cec1eb421ab84f6bb2257c0f61495407492fb3
dimasmsoares <35304959+dimasmsoares@users.noreply.github.com>
2026-02-10 13:45:22 +0000

    10-02-2026 1

--- main.c
+++ main.c
@@ -2,11 +2,81 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /*PROTOTIPOS*/
-int get_key_press()
+int get_key_press();
+
+typedef struct{
+    char *campos[3];
+    char *valores[3];
+    int n_opcoes;
+} Form;
 
 int main(){
+
+    int selecionado = 0;
+    //char *opcoes[] = {"Nome: ", "Endereço: ", "Nível: "};
+    //int n_opcoes = 3;
+    Form form = {
+    .campos = {"Nome: ", "Endereço: ", "Nível: "},
+    .valores = {"Dimas Moreira Soares", "SGAN 914 MODULO A", "2"},
+    .n_opcoes = 3
+    };
+
+    while(1){
+
+        system("clear");
+        printf("\033[?25l");
+
+        for(int i = 0; i < form.n_opcoes; i++){
+            if(selecionado == i){
+                printf(">\t%s%s\n", form.campos[i], form.valores[i]);
+            }
+            else{
+                printf("\t%s%s\n", form.campos[i], form.valores[i]);
+            }
+        }
+
+        int ch = get_key_press();
+        /*Quando pressionamos uma tecla que não pertence a tabela ASCII, o computador envia não um, mas 3 bytes em sequência para
+        para nosso programa:
+        27 (ESC): É o caractere "Escape". Ele serve como um alerta.
+        91 ([): Na tabela ASCII, o 91 é o abre colchetes. Nos padrões de terminal (ANSI/VT100), o ESC seguido de [ inicia a maioria 
+        dos comandos de movimentação de cursor.
+        XX (): Um byte, que especifica o comando. Ex: (66) = seta para baixo ↓ | (65) = seta para cima ↑*/
+
+        // Verifica se é uma seta de es uma seta "↑" "↓" ou "ENTER".
+        if(ch == 27){   // Escape (ESC)
+            ch = get_key_press();
+            if(ch == 91){
+                ch = get_key_press();
+                if(ch == 65){   // Seta para cima
+                    if(selecionado > 0){
+                        selecionado --;
+                    }
+                    else{
+                        selecionado = form.n_opcoes - 1;
+                    }
+                } // if(ch == 65)
+                else if(ch == 66){  // Seta para baixo
+                    if(selecionado < (form.n_opcoes -1)){
+                        selecionado ++;
+                    }
+                    else{
+                        selecionado = 0;
+                    }
+                } // else if(ch == 66)
+            } // if(ch == 91)
+        } // if(ch == 27)
+
+        if(ch == 10){   // ENTER
+            printf("\n%d\n", selecionado);
+            break;
+        }
+    }
+    printf("\033[?25h");
+
     return 0;
 }
 
